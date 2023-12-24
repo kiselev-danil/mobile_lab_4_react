17d721351bd1f3da2a3f11994f2aef6add28dfa4
Kiselev Danil <dabikis2855@gmail.com>
2023-12-24 14:53:08 +0600

    [~] import few fonts and AppHeader module

---
+++ src/components/AppHeader/ApppHeader.tsx
@@ -0,0 +1,12 @@
+import classes from "./AppHeader.module.css"
+import clsx from "clsx";
+
+const AppHeader: React.FC = () => {
+    return (
+        <p>
+            Hello, header
+        </p>
+    )
+}
+
+export default AppHeader
