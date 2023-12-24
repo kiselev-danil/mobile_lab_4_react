0806c8f27bf4a6317291a7f60ece64f6442f3b9c
Kiselev Danil <dabikis2855@gmail.com>
2023-12-24 22:57:48 +0600

    [+] add consultation advert

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,9 @@ function App() {
       <div className={classes.appHeader}>
         <AppHeader />
       </div>
-      <ConsultAdv />
+      <div className={classes.consultAdv}>
+        <ConsultAdv />
+      </div>
     </>
   )
 }
---
+++ src/components/ConsultAdv/ConsultAdv.tsx
@@ -0,0 +1,25 @@
+
+import classes from "./ConsultAdv.module.css"
+import clsx from "clsx";
+import consultIllustrationSRC from '../../assets/illustration1.svg'
+
+const ConsultAdv: React.FC = () => {
+    return (
+        <div className={classes.consultAdv}>
+            <div className={classes.consultBody}>
+                <h2 className={classes.blockLabel}>Virtual healthcare for you</h2>
+                <p className={classes.text}>Our service provides progressive, and affordable healthcare, accessible on mobile and online for everyone</p>
+                <button className={classes.consultButton}>
+                    Consult today
+                </button>
+            </div>
+            <div className={classes.consultIllustration}>
+                <img src={consultIllustrationSRC} />
+
+            </div>
+
+        </div>
+    )
+}
+
+export default ConsultAdv
