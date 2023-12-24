b9c9ae4ccc3a9076f45ca54d0fa783cbf0181801
Kiselev Danil <dabikis2855@gmail.com>
2023-12-24 20:54:03 +0600

    [~] move margin property into app css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import classes from './App.module.css'
 import AppHeader from './components/AppHeader'
-// import './App.css'
+import ConsultAdv from './components/ConsultAdv'
+import clsx from "clsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <>
-        <AppHeader/>
-      </>
+    <>
+      <div className={classes.appHeader}>
+        <AppHeader />
+      </div>
+      <ConsultAdv />
+    </>
   )
 }
 
